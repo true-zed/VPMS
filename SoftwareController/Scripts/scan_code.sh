16e202634e91f10c9516748a663d78de2cc9fe85
Zakhar Kotelnikov <dev@zed-software.ru>
2021-04-12 06:31:07 +0300

    Update scan_code.sh

--- SoftwareController/Scripts/scan_code.sh
+++ SoftwareController/Scripts/scan_code.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-echo Connect to adb
+echo "Connecting to adb .."
 adb devices
 
-echo Starting WhatsApp Business
+echo "Opening the WhatsApp Business .."
 adb shell am start -n com.whatsapp.w4b/com.whatsapp.Main
 
-echo Waiting to load Whatsapp
+echo "Waiting for WhatsApp Business to load .."
 sleep 3
 
-echo Opening Menu
+echo "Opening the menu .."
 adb shell input tap 300 30
+
+echo "Waiting for the menu to open .."
 sleep 2
 
-echo Opening WhatsApp Web tab
+echo "Opening the \"WhatsApp Web\" tab .."
 adb shell input tap 160 245
+
+echo "Waiting for the tab to open .."
 sleep 2
 
-echo Adding new web session
+echo "Adding a new web session .."
 adb shell input tap 160 310
+
+echo "Waiting for a web session to be added .."
 sleep 1
 
-echo Taping to Ok at instruction
+echo "Tapping the \"OK\" button in the instructions .."
 adb shell input tap 160 535
 
-echo Waiting to scan code
-sleep 8
+echo "Waiting for a code scan .."
+sleep 10
 
-echo Close WhatsApp Business
+echo "Scanning completed!"
+
+echo "Closing WhatsApp Business .."
 adb shell am force-stop com.whatsapp.w4b
-
-echo Done!
