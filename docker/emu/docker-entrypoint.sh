27f9f129b965d1a5e2e158a5eae656cac17479d6
true-zed <dev@zed-software.ru>
2021-04-22 00:18:32 +0000

    Reworked Docker

--- docker/emu/docker-entrypoint.sh
+++ docker/emu/docker-entrypoint.sh
@@ -4,11 +4,18 @@ set -e
 
 rm -f /opt/android-sdk-linux/.android/avd/bot.avd/*.lock
 
-emulator -avd bot -no-window -no-audio -no-boot-anim -camera-back webcam0 > /dev/null 2>&1 < /dev/null
+emulator -avd bot -no-window -no-audio -no-boot-anim -camera-back webcam0 &
 
-adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+adb wait-for-device shell 'while [[ -z $(getprop dev.bootcomplete) ]]; do sleep 1; done;'
 
-if ! adb shell pm list packages | grep com.whatsapp.w4b
-then adb install /opt/android-sdk-linux/apks/wab.apk
+echo "Emulator booted!"
+
+sleep 60
+if ! adb shell pm list packages | grep com.whatsapp.w4b; then
+adb install /opt/android-sdk-linux/apks/wab.apk
+adb shell pm grant com.whatsapp.w4b android.permission.CAMERA
 fi
 
+echo "WhatsApp Business Installed!"
+
+tail -f /dev/null
